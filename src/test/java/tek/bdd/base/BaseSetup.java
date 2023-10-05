230429b1aeaa746e2e30bd11d94333bee146d730
Sakhi Hamidi <sakhihamidi@gmail.com>
2023-10-05 16:19:43 -0400

    adding new comments

--- src/test/java/tek/bdd/base/BaseSetup.java
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -14,6 +14,7 @@ import java.util.Properties;
 import java.util.function.BinaryOperator;
 
 public class BaseSetup {
+    // Adding some comments
 
     //Encapsulation Concept of Java
     private static WebDriver driver;
